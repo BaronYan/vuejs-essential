11e4cf7deda43f8cf3e02dcb1c4545b5cd310a10
Baronyan <1229598328@qq.com>
2020-11-03 20:49:40 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to here</h1>
   </div>
 </template>
 
@@ -23,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 200px;
 }
 </style>
